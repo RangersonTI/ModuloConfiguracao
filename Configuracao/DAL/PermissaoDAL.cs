60a6286e2959ab1cb28091ee3ef1501af92d069d
Rangerson Clemente <rangerson14@outlook.com>
2023-03-14 16:45:04 -0300

    atualizando...

---
+++ Configuracao/DAL/PermissaoDAL.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class PermissaoDAL
+    {
+        public void Inserir(Permissao _permissao)
+        {
+
+        }
+
+
+        public void Atualizar(Permissao _permissao)
+        {
+
+        }
+
+        public void Deletar(int _id)
+        {
+
+        }
+
+        public List<Permissao> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Permissao> BuscarporDescrcao(string _nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Permissao> BuscarporId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+
+        }
+
+        public List<Usuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> BuscarporNome(string _nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> BuscarporId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Usuario _usuario)
+        {
+
+        }
+        public void Deletar(Usuario _usuario)
+        {
+
+        }
+    }
+}
